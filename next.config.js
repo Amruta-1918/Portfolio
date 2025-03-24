5437de0dd10f5804d429a774926fd87acff0cd13
Amruta-1918 <cyberclub1815@gmail.com>
2025-03-24 20:30:08 +0530

    Add GitHub Pages deployment configuration

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/Portfolio',
+  assetPrefix: '/Portfolio',
 }
 
 module.exports = nextConfig 
